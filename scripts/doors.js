383e37fedd50a5128a5fc783935d26e89c5a15bb
Andrew Pamment <apamment@positronicbrain.net>
2024-11-19 19:39:33 +1000

    improve doors

--- scripts/doors.js
+++ scripts/doors.js
@@ -9,35 +9,41 @@ function pad(pad, str, padLeft) {
 
 var doors = getdoors()
 
-while (true) {
+if (doors.length == 0) {
     cls()
-    if (!gfile("doors")) {
-        print("D O O R S\r\n\r\n");
-    }
-
-    var lines = 0
-
-    for (var index = 0; index < doors.length; ++index) {
-        var element = doors[index];
-        print("|15" + pad("   ", index + 1, true) + ". |10" + pad("                                ", element['name'], false) + " |13" + pad("     ", Number(load(element['key'], "0")), true) + " PLAYS|07\r\n")
-        lines = lines + 1
-        if (lines == 20) {
-            pause()
-            lines = 0
+    print("Sorry, no doors are installed on this system...\r\n")
+    pause()
+} else {
+    while (true) {
+        cls()
+        if (!gfile("doors")) {
+            print("D O O R S\r\n\r\n");
         }
-    }
-    print("\r\n|15  Q. |10Quit to Main Menu\r\n");
-    print("\r\n|08:> |07")
-    var ch = gets(3)
 
-    if (ch == "q" || ch == "Q") {
-        break
-    } else {
-        var num = Number(ch) - 1;
-        if (num >= 0 && num < doors.length) {
-            rundoor(doors[num]['key'])
-            plays = Number(load(doors[num]['key'], "0")) + 1;
-            save(doors[num]['key'], plays)
+        var lines = 0
+
+        for (var index = 0; index < doors.length; ++index) {
+            var element = doors[index];
+            print("|15" + pad("   ", index + 1, true) + ". |10" + pad("                                ", element['name'], false) + " |13" + pad("     ", Number(load(element['key'], "0")), true) + " PLAYS|07\r\n")
+            lines = lines + 1
+            if (lines == 20) {
+                pause()
+                lines = 0
+            }
+        }
+        print("\r\n|15  Q. |10Quit to Main Menu\r\n");
+        print("\r\n|08:> |07")
+        var ch = gets(3)
+
+        if (ch == "q" || ch == "Q") {
+            break
+        } else {
+            var num = Number(ch) - 1;
+            if (num >= 0 && num < doors.length) {
+                rundoor(doors[num]['key'])
+                plays = Number(load(doors[num]['key'], "0")) + 1;
+                save(doors[num]['key'], plays)
+            }
         }
     }
 }
