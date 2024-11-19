a7e03a44c51114b6352a3ef5b7f1c15aaa0bf220
Andrew Pamment <apamment@positronicbrain.net>
2024-11-19 17:35:04 +1000

    Fix typo

--- scripts/doors.js
+++ scripts/doors.js
@@ -18,7 +18,7 @@ while (true) {
         var element = doors[index];
         print("|15" + pad("   ", index + 1, true) + ". |10" + pad("                                ", element['name'], false) + " |13" + pad("     ", Number(load(element['key'], "0")), true) + " PLAYS|07\r\n")
     }
-    print("|15  Q. |10Quit to Main Menur\r\n");
+    print("\r\n|15  Q. |10Quit to Main Menu\r\n");
     print("\r\n|08:> |07")
     var ch = gets(3)
 
