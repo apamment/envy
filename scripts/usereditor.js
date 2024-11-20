dace949c6db716472aea90729807b9197b0cc834
Andrew Pamment <apamment@positronicbrain.net>
2024-11-20 21:33:18 +1000

    work on usereditor

--- scripts/usereditor.js
+++ scripts/usereditor.js
@@ -5,8 +5,8 @@ while (true) {
 
     cls()
     print("User Editor - " + users[index]['username'] + "(" + users[index]['uid'] + ")\r\n\r\n")
-    print("(F)ullName: " + getattro(users[index]['uid'], "fullname", "UNKNOWN") + "\r\n")
-    print("(S)ecLevel: " + getattro(users[index]['uid'], "seclevel", "10") + "\r\n")
+    print("(F)ullName: " + getattro(users[index]['uid'].toString(), "fullname", "UNKNOWN") + "\r\n")
+    print("(S)ecLevel: " + getattro(users[index]['uid'].toString(), "seclevel", "10") + "\r\n")
     print("(N)ext, (P)revious, (Q)uit\r\n")
     var ch = getch()
 
@@ -18,18 +18,18 @@ while (true) {
     } else if (ch == 'n' || ch == 'N') {
         index = index + 1
         if (index >= users.length) {
-            index = users.lenfth -1
+            index = users.length - 1
         }
     } else if (ch == 'f' || ch == 'F') {
         print("New SecLevel: ")
         var sl = Number(gets(3))
         if (sl > 0 && sl <= 99) {
-            putattro(users[index]['uid'], "seclevel", sl)
+            putattro(users[index]['uid'].toString(), "seclevel", sl)
         }
     } else if (ch == 's' || ch == 'S') {
         print("New FullName: ")
         var fn = gets(65)
-        putattro(users[index]['uid'], "fullname", fn)
+        putattro(users[index]['uid'].toString(), "fullname", fn)
     } else if (ch == 'q' || ch == 'Q') {
         break;
     }
