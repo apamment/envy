73dbf1b2bc29ad5c56a4f84e89548fad1596c5ce
Andrew Pamment <apamment@positronicbrain.net>
2024-11-20 21:56:54 +1000

    more details

--- scripts/usereditor.js
+++ scripts/usereditor.js
@@ -6,6 +6,8 @@ while (true) {
     cls()
     print("User Editor - " + users[index]['username'] + "(" + users[index]['uid'] + ")\r\n\r\n")
     print("(F)ullName: " + getattro(Number(users[index]['uid']), "fullname", "UNKNOWN") + "\r\n")
+    print("(L)ocation: " + getattro(Number(users[index]['uid']), "location", "Somewhere, The World") + "\r\n")
+    print("(E)mail: " + getattro(Number(users[index]['uid']), "email", "UNKNOWN") + "\r\n")
     print("(S)ecLevel: " + getattro(Number(users[index]['uid']), "seclevel", "10") + "\r\n")
     print("(N)ext, (P)revious, (Q)uit\r\n")
     var ch = getch()
@@ -30,6 +32,14 @@ while (true) {
         print("New FullName: ")
         var fn = gets(65)
         putattro(Number(users[index]['uid']), "fullname", fn)
+    } else if (ch == 'l' || ch == 'L') {
+        print("New Location: ")
+        var lc = gets(32)
+        putattro(Number(users[index]['uid']), "location", lc)
+    } else if (ch == 'e' || ch == 'E') {
+        print("New Email: ")
+        var em = gets(32)
+        putattro(Number(users[index]['uid']), "email", em)        
     } else if (ch == 'q' || ch == 'Q') {
         break;
     }
