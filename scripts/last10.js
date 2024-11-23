49ca55fbe06c5c91cc0098a615654e9300787114
Andrew Pamment <apamment@positronicbrain.net>
2024-11-23 12:45:40 +1000

    dont include sysop in last callers

--- scripts/last10.js
+++ scripts/last10.js
@@ -27,14 +27,16 @@ for (var i = 0; i < 10; i++) {
 var thetime = new Date();
 var call_count = Number(load("call-count", "0")) + 1;
 var finalline = "|07" + pad("      ", call_count, true) + ". |15" + pad("                 ", getusername(), false) + " |10" + pad("                                ", getattr("location", "Somewhere, The World"), false) + " |13" + thetime.toDateString() + "|07"
-save("call-count", call_count)
 
+if (getusername() != opname()) {
+    save("call-count", call_count)
 
-for (var i = 1; i < 10; i++) {
-    save("line-" + i, lines[i-1])
+    for (var i = 1; i < 10; i++) {
+        save("line-" + i, lines[i-1])
+    }
+
+    save("line-0", finalline)
+
+    print("\r\n")
 }
-
-save("line-0", finalline)
-
-print("\r\n")
 pause()
